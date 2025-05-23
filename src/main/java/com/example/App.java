0b32809e18356d69b85ee9e16478358f1ff553fe
TejashreeNv <ubuntu@ip-172-31-9-92.ap-south-1.compute.internal>
2025-05-23 10:22:12 +0000

    Added complete Maven project with pom.xml, src, and target directory

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        int sum = 2 + 3;
+        System.out.println("The sum of 2 + 3 is: " + sum);
+    }
+}
