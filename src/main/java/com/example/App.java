6121fde8112c1dd2a24bd4a8dfdf40df922a4bbe
Tejashree N V <170778022+TejashreeNv@users.noreply.github.com>
2025-05-23 15:11:55 +0530

    Delete src/main/java/com/example directory

--- src/main/java/com/example/App.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        int sum = 2 + 3;
-        System.out.println("The sum of 2 + 3 is: " + sum);
-    }
-}
